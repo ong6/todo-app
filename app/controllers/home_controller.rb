5ff0943ffd3a967a9e4dd942fdd90e46ced17485
ong6 <junxiongong2@gmail.com>
2021-01-30 02:50:43 +0800

    First commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+    def trash_em
+        @trash_em = List.where(:completed => true).destroy_all
+        redirect_to root_url, notice: "All Marked Items Were Successfully Deleted"
+    end
+
+    def trash_em_all 
+        @trash_em_all = List.all.destroy_all
+        redirect_to root_url, notice: "All Items Were Successfully Deleted"
+    end
+
+end
